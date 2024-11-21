4b9018a8760a3d65592ef7ad4bc6fe85c74e9828
[surferjreb] <[surferjreb@live.com]>
2024-11-21 00:18:52 -0500

    created product page

--- app.py
+++ app.py
@@ -3,13 +3,42 @@
     By: James R. Brown
 '''
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
+from store_items import StoreItems
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def index():
-    return render_template('index.html', title="EComStore", message="You Made it")
+    return render_template('index.html', title="EComStore", message="Welcome")
+
+@app.route("/products")
+def products():
+    items = StoreItems().STORE_ITEMS
+    return render_template('products.html', title="products", items=items)
+
+@app.route("/products/<item>")
+def product_view(item):
+    pass
+
+@app.route("/login")
+def login():
+    pass
+
+@app.route("/cart")
+def view_cart():
+    pass
+
+@app.route("/cart/add_to_cart/<int:id>")
+def add_to_cart(item_id):
+    pass
+
+@app.route("/checkout")
+def checkout():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ store_items.py
@@ -0,0 +1,12 @@
+from flask import url_for
+
+class StoreItems:
+
+    STORE_ITEMS = {1: ('collection', 750,'./static/images/store_items/IMG_0652.JPG'),
+                   2: ('flower 1', 150, './static/images/store_items/IMG_0655.JPG'),
+                   3: ('flower 2', 200, './static/images/store_items/IMG_0656.JPG'),
+                   4: ('flower 3', 350, './static/images/store_items/IMG_0657.JPG'),
+                   5: ('flower 4', 100, './static/images/store_items/IMG_0658.JPG'),
+                   6: ('flower 5', 400, './static/images/store_items/IMG_0659.JPG'),
+                   7: ('flower 6', 150, './static/images/store_items/IMG_0660.JPG'),
+                  }
